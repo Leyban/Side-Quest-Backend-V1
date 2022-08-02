3a88dc27546f21bcce66082cab2f883f2731111b
Leyban Lazada <leybanlazada@gmail.com>
2022-08-03 00:11:45 +0800

    added corsOptions

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ const start = async () => {
     const app = express()
     const httpServer = http.createServer(app)
     const cors = require('cors')
+    const corsOptions ={
+      origin:'*', 
+      credentials:true,            //access-control-allow-credentials:true
+      optionSuccessStatus:200,
+    }
 
     const schema = makeExecutableSchema({ typeDefs, resolvers})
 
@@ -95,7 +100,7 @@ const start = async () => {
         path: '/',
     });
 
-    app.use(cors())
+    app.use(cors(corsOptions))
 
     const PORT = process.env.PORT || 4000
 
